f463e719f3d48713eae6e5459266440c60454feb
Parinaz <parinaz@ug.bilkent.edu.tr>
2023-12-12 02:21:04 +0330

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // Add the dependency for the Google services Gradle plugin
+    id 'com.google.gms.google-services' version '4.4.0' apply false
+}
